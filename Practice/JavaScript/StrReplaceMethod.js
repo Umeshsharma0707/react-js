67742a45c8cb02a807c04b2e43615673d404e05a
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-01 22:52:52 +0530

    practice

---
+++ Practice/JavaScript/StrReplaceMethod.js
@@ -0,0 +1,8 @@
+// Define a string variable 'str' 
+let str = "Mind, Power, Soul";
+
+// Use the replace() method to replace the substring
+let part = str.replace("Power", "Space");
+
+// Output the resulting string after replacement
+console.log(part);
